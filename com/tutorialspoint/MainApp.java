4dea381dc40621f0097c4e22fcee690c617e2e75
Ford Tang <FordTang@gmail.com>
2016-07-22 13:43:24 -0700

    Injecting Collection

--- com/tutorialspoint/MainApp.java
+++ com/tutorialspoint/MainApp.java
@@ -7,12 +7,21 @@ public class MainApp {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		
-		TextEditor te = (TextEditor) context.getBean("textEditor");
-
-		te.spellCheck();
+		JavaCollection jc = (JavaCollection)context.getBean("javaCollection");
+		
+		jc.getAddressList();
+		jc.getAddressSet();
+		jc.getAddressMap();
+		jc.getAddressProp();
 		
 		/*
-		HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+		TextEditor te = (TextEditor)context.getBean("textEditor");
+
+		te.spellCheck();
+		*/
+		
+		/*
+		HelloWorld objA = (HelloWorld)context.getBean("helloWorld");
 		
 		objA.getMessage1();
 		objA.getMessage2();
